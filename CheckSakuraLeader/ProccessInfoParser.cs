8abeacba5ed20cf22cf2df42b6d18f0c22be36af
Mineflat <mineflat@yandex.ru>
2023-09-10 21:54:40 +0300

    Добавьте файлы проекта.

---
+++ CheckSakuraLeader/ProccessInfoParser.cs
@@ -0,0 +1,44 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+namespace CheckSakuraLeader
+{
+    internal static class ProccessInfoParser
+    {
+        private static HttpClient httpClient = new ();
+        /// <summary>
+        /// Проверка, является ли сервер с указанным UUID лидером кластера
+        /// </summary>
+        /// <param name="URI">Адрес, используемый для получения отладочной информации</param>
+        /// <param name="targetUUID">UUID предполагаемого лидера кластера</param>
+        public static void CheckServerLeader(string URI, string targetUUID)
+        {
+            string serverResponce = GetRequestBody(URI).Result;
+            dynamic data = JObject.Parse(serverResponce);
+            for (int i = 0; i < data.nodes.Count; i++)
+            {
+                if (data.nodes[i].uid == targetUUID)
+                {
+                    if (data.nodes[i].state == "master" && data.nodes[i].isMaster == true)
+                        Environment.Exit(0);
+                    else
+                        Environment.Exit(1);
+                }
+            }
+        }
+        /// <summary>
+        /// Выполнение HTTP GET-запроса по указанному URI
+        /// </summary>
+        /// <param name="URI">URI, на который будет отправлен запрос</param>
+        /// <returns></returns>
+        private static async Task<string> GetRequestBody(string URI)
+        {
+            HttpClientHandler clientHandler = new()
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
+            };
+            HttpClient client = new(clientHandler);
+            return await client.GetStringAsync(URI);
+        }
+    }
+}
---
+++ CheckSakuraLeader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace CheckSakuraLeader
+{
+    internal class Program
+    {
+        private static readonly string ServerDebugURI = "https://127.0.0.1:1322";
+        private static readonly string ServerProccessName = "*";
+        static void Main(string[] args)
+        {
+            // Рассматриваем вариант, когда конкретно на этой ВМ вообще сервер Сакура не запущен
+            Process[] sakuraProccess = Process.GetProcessesByName(ServerProccessName);
+            if(sakuraProccess.Length== 0) Environment.Exit(1);
+            // Определяем мастера сервера, исходя из переданного UUID
+            if (args.Length != 1) Environment.Exit(1);
+            ProccessInfoParser.CheckServerLeader(ServerDebugURI, args[0]);
+        }
+    }
+}
